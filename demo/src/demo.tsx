e8b40b0e15960392c8bc904f23e85a3c83be0623
Marat Abdullin <marata@microsoft.com>
2020-05-02 00:46:36 +0200

    A better example.

--- demo/src/demo.tsx
+++ demo/src/demo.tsx
@@ -12,68 +12,134 @@ setupAbilityHelpers(window);
 const AH = getAbilityHelpers();
 AH.outline.setup();
 
-function Item(props: React.Props<{}>) {
-    return (
-        <div
-            tabIndex={0}
-            className='item'
-            { ...getAbilityHelpersAttribute({ groupper: {
-                isLimited: AHTypes.GroupperFocusLimit.LimitedTrapFocus
-            }})}
-        >
-            { props.children
-                ? props.children
-                : ([
-                    <button onClick={() => { alert('Ololo'); }}>Hello</button>,
-                    <button onClick={() => { alert('Piupiu'); }}>World</button>
-                ])
-            }
-        </div>
-    );
+class App extends React.PureComponent {
+    private _modal: Modal | undefined;
+
+    render() {
+        return (
+            <div { ...getAbilityHelpersAttribute({ root: {} }) }>
+                <div aria-label='Main' { ...getAbilityHelpersAttribute({ modalizer: { id: 'main' }, deloser: {} }) }>
+                    <h1>Hello world</h1>
+
+                    <div>
+                        <Item onClick={ this._onClick } />
+                        <Item onClick={ this._onClick } />
+
+                        <Item onClick={ this._onClick }>
+                            <div>
+                                <Item onClick={ this._onClick } />
+
+                                <Item onClick={ this._onClick }>
+                                    <div>
+                                        <Item onClick={ this._onClick } />
+                                        <Item onClick={ this._onClick } />
+
+                                        <Item onClick={ this._onClick }>
+                                            <div>
+                                                <Item onClick={ this._onClick } />
+                                                <Item onClick={ this._onClick } />
+                                                <Item onClick={ this._onClick } />
+                                            </div>
+                                        </Item>
+
+                                        <Item onClick={ this._onClick } />
+                                        <Item onClick={ this._onClick } />
+                                    </div>
+                                </Item>
+
+                                <Item onClick={ this._onClick } />
+                            </div>
+                        </Item>
+
+                        <Item onClick={ this._onClick } />
+                        <Item onClick={ this._onClick } />
+                    </div>
+                </div>
+
+                <Modal ref={ this._onModalRef } />
+            </div>
+        );
+    }
+
+    private _onModalRef = (ref: Modal | null) => {
+        this._modal = ref || undefined;
+    }
+
+    private _onClick = () => {
+        if (this._modal) {
+            this._modal.show();
+        }
+    }
 }
 
-ReactDOM.render(
-    <div { ...getAbilityHelpersAttribute({ root: {}, deloser: {} }) }>
-        <h1>Hello world</h1>
+class Item extends React.PureComponent<{ onClick: () => void }> {
+    render() {
+        return (
+            <div
+                tabIndex={0}
+                className='item'
+                { ...getAbilityHelpersAttribute({ groupper: {
+                    isLimited: AHTypes.GroupperFocusLimit.LimitedTrapFocus
+                }})}
+            >
+                { this.props.children
+                    ? this.props.children
+                    : ([
+                        <button onClick={ this.props.onClick }>Hello</button>,
+                        <button onClick={ this.props.onClick }>World</button>
+                    ])
+                }
+            </div>
+        );
+    }
+}
 
-        <p><button>Hello</button></p>
+class Modal extends React.PureComponent<{}, { isVisible: boolean }> {
+    private _div: HTMLDivElement | undefined;
 
-        <div style={ { overflow: 'scroll', height: '600px' } }>
-            <Item />
-            <Item />
+    constructor(props: {}) {
+        super(props);
+        this.state = { isVisible: false };
+    }
 
-            <Item>
-                <div>
-                    <Item />
+    render() {
+        if (!this.state.isVisible) {
+            return null;
+        }
 
-                    <Item>
-                        <div>
-                            <Item />
-                            <Item />
-
-                            <Item>
-                                <div>
-                                    <Item />
-                                    <Item />
-                                    <Item />
-                                </div>
-                            </Item>
-
-                            <Item />
-                            <Item />
-                        </div>
-                    </Item>
-
-                    <Item />
+        return (
+            <div>
+                <div className='lightbox'></div>
+                <div ref={ this._onRef } aria-label='Modal' role='region' className='modal'>
+                    <h3>Piu piu</h3>
+                    <button onClick={ this._onBtnClick }>Close</button>
+                    &nbsp;or&nbsp;
+                    <button onClick={ this._onBtnClick }>Dismiss</button>
                 </div>
-            </Item>
+            </div>
+        );
+    }
 
-            <Item />
-            <Item />
-        </div>
+    show() {
+        this.setState({ isVisible: true });
+    }
 
-        <p><button>Hello</button></p>
-    </div>,
+    private _onRef = (el: HTMLDivElement | null) => {
+        if (el) {
+            this._div = el;
+            AH.modalizer.add(el, { id: 'modal' });
+            AH.deloser.add(el);
+            AH.modalizer.focus(el);
+        } else if (this._div) {
+            AH.modalizer.remove(this._div);
+            AH.deloser.remove(this._div);
+            this._div = undefined;
+        }
+    }
 
-    document.getElementById('demo')
-);
+    private _onBtnClick = () => {
+        this.setState({ isVisible: false });
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('demo'));
