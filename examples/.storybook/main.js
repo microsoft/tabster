fd5a8cb36fd165e9e35e446661f574b13e6e5241
Marat Abdullin <marata@microsoft.com>
2021-03-01 14:12:39 +0100

    Fixing the build and the isDev environment.

--- examples/.storybook/main.js
+++ examples/.storybook/main.js
@@ -11,10 +11,12 @@ module.exports = {
   webpackFinal: async (config, type) => {
     config.resolve.plugins.push(new TsconfigPathsPlugin({ extensions: config.resolve.extensions }));
 
-    const isDev = type === 'DEVELOPMENT';
+    const isDev = (process.env.NODE_ENV !== 'production');
+
     config.plugins.push(new webpack.DefinePlugin({
       __DEV__: JSON.stringify(isDev),
     }));
-    return config
+
+    return config;
   }
 }
