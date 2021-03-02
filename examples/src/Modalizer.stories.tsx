26a98a4230ffed9c956fce48da82dcde3999145b
Lingfan Gao <lingfan.gao@microsoft.com>
2021-03-02 13:17:12 +0000

    initialize APIs for all stories

--- examples/src/Modalizer.stories.tsx
+++ examples/src/Modalizer.stories.tsx
@@ -1,25 +1,11 @@
 import * as React from 'react';
 import { Meta } from '@storybook/react';
-import { getAbilityHelpersAttribute, getCurrentAbilityHelpers, getDeloser, getModalizer } from 'ability-helpers';
+import { getAbilityHelpersAttribute } from 'ability-helpers';
 import { Modal } from './components/Modal';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     title: 'Modalizer',
-    decorators: [
-        (Story) => {
-            const ah = getCurrentAbilityHelpers(window);
-            
-            // These APIs need to initialized before use, no reason to do it for all stories yet
-            if (ah) {
-                getModalizer(ah);
-                getDeloser(ah);
-            }
-
-            return <Story />;
-        }
-    ]
-  
 } as Meta;
 
 export const ModalDialog = () => {
