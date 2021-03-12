215b9ed62745959aa29b6bc643ca566feee2c1f9
Lingfan Gao <lingfan.gao@microsoft.com>
2021-03-12 15:56:04 +0000

    Add keyboard navigation state example
    Adds a simple navigation to see if an element was focused with keyboard
    navigation or mouse

---
+++ examples/src/KeyboardNavigationState.stories.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { getCurrentAbilityHelpers} from 'ability-helpers';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'KeyboardNavigationState',
+};
+
+export const IsFocusingWithKeyboard = () => {
+    const [message, setMessage] = React.useState('undetermined');
+    const onFocus = () => {
+        const ah = getCurrentAbilityHelpers(window);
+        const isKeyboard = ah?.keyboardNavigation.isNavigatingWithKeyboard();
+        if (isKeyboard) {
+            setMessage('keyboard')
+        } else {
+            setMessage('mouse');
+        }
+
+    }
+    return (
+        <>
+            <div>Focused with: <strong>{message}</strong></div>
+            <button onFocus={onFocus}>Click or use keyboard</button>
+        </>
+    )
+};
