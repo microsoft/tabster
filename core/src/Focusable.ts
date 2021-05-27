microsoft/tabster

commit 9322f30ae0463199799c9b960f3bb98650e3bb97

Remove modalizer accessibility and set aria-hidden in tree instead (#27)
* Unique modalizer instance per user id

This PR refactors modalizer so that one single instance exists and
manages and Modalizer DOM elements per user id. This makes sense since
all modalizers with the same id need to behave the same way.

This will enable more logic to be moved from `Root` to `Modalizer` in
the future. Lots of logic currently in `Root` regarding modalizer exists
because of the limitation that multiple modalizer instances can exist
that have the same user id.

* add comment

* simplify root

* update root

* remove nodefilter

* Experiment: Remove modalizer accessibility

This PR refactors modalizers to set `aria-hidden` on all nodes that do
not belong to the specific modalizer.

Removes the concept of accessibility of modalizers and cleans up Root
API to be unaware of modalizers

* comment modalizer usage in deloser

* cleanup more modalizers

* lint and comments

* remove modalizer timer

* improve story

* improve story

* modalizer obeserve mutations

* perf optimization

* remove modalizer ref from Focusable

* add comments

* uncomment code

* chage modalizer to single element and stop using main modalizers

* add modalizer conditions to accept element

* remove useless import

* leverage curModalizer

* lint

* extract fuction

* add comments

* enhance stories

* enhance stories

* enhance stories

* fix restore focus bug

* rename variable

* rename fix

* Remove active modalizer on mutation

* improve comments

* add isOthersAccessible example

* simplify tree walking

* don't use imperative add APIs

core/src/Focusable.ts
@@ -766,7 +766,7 @@ export class FocusableAPI implements Types.FocusableAPI {
     private _getGroupFirst(groupElement: HTMLElement, ignoreGroupper: boolean): HTMLElement | null {
         return this._tabster.focusable.isFocusable(groupElement)
             ? groupElement
-            : this._tabster.focusable.findFirst(groupElement, false, false, ignoreGroupper);
+            : this._tabster.focusable.findFirst(groupElement, false, ignoreGroupper);
     }
 
     addGroupper(element: HTMLElement, basic?: Types.GroupperBasicProps, extended?: Types.GroupperExtendedProps): void {
@@ -926,7 +926,7 @@ export class FocusableAPI implements Types.FocusableAPI {
                         groupperTabsterOnElement.groupper &&
                         (ignoreModalizer || this.isAccessible(el as HTMLElement))
                     ) {
-                        if (!this._tabster.focusable.isFocusable(el) && !this._tabster.focusable.findFirst(el, false, false, true)) {
+                        if (!this._tabster.focusable.isFocusable(el) && !this._tabster.focusable.findFirst(el, false, true)) {
                             continue;
                         }
 
@@ -1049,20 +1049,13 @@ export class FocusableAPI implements Types.FocusableAPI {
     isAccessible(el: HTMLElement): boolean {
         for (let e: (HTMLElement | null) = el; e; e = e.parentElement) {
             const tabsterOnElement = getTabsterOnElement(this._tabster, e);
-
-            if (tabsterOnElement && tabsterOnElement.modalizer && !tabsterOnElement.modalizer.isActive()) {
-                return true;
-            }
-
-            let attrVal = e.getAttribute('aria-hidden');
-
-            if (attrVal && (attrVal.toLowerCase() === 'true')) {
+            if (this._isHidden(e)) {
                 return false;
             }
 
-            attrVal = e.getAttribute('aria-disabled');
             const ignoreDisabled = tabsterOnElement?.focusable?.ignoreAriaDisabled;
-            if (!ignoreDisabled && attrVal && (attrVal.toLowerCase() === 'true')) {
+
+            if (!ignoreDisabled && this._isDisabled(e)) {
                 return false;
             }
         }
@@ -1070,17 +1063,33 @@ export class FocusableAPI implements Types.FocusableAPI {
         return true;
     }
 
+    private _attrIs(el: HTMLElement, name: string, value: string): boolean {
+        let attrVal = el.getAttribute(name);
+        if (attrVal && (attrVal.toLowerCase() === value)) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    private _isDisabled(el: HTMLElement): boolean {
+        return this._attrIs(el, 'aria-disabled', 'true');
+    }
+
+    private _isHidden(el: HTMLElement): boolean {
+        return this._attrIs(el, 'aria-hidden', 'true');
+    }
+
     findFirst(
         context?: HTMLElement,
         includeProgrammaticallyFocusable?: boolean,
-        ignoreModalizer?: boolean,
         ignoreGroupper?: boolean
     ): HTMLElement | null {
         return this._findElement(
             context || this._getBody(),
             null,
             includeProgrammaticallyFocusable,
-            ignoreModalizer,
             ignoreGroupper,
             false
         );
@@ -1089,14 +1098,12 @@ export class FocusableAPI implements Types.FocusableAPI {
     findLast(
         context?: HTMLElement,
         includeProgrammaticallyFocusable?: boolean,
-        ignoreModalizer?: boolean,
         ignoreGroupper?: boolean
     ): HTMLElement | null {
         return this._findElement(
             context || this._getBody(),
             null,
             includeProgrammaticallyFocusable,
-            ignoreModalizer,
             ignoreGroupper,
             true
         );
@@ -1106,14 +1113,12 @@ export class FocusableAPI implements Types.FocusableAPI {
         current: HTMLElement,
         context?: HTMLElement,
         includeProgrammaticallyFocusable?: boolean,
-        ignoreModalizer?: boolean,
         ignoreGroupper?: boolean
     ): HTMLElement | null {
         return this._findElement(
             context || this._getBody(),
             current,
             includeProgrammaticallyFocusable,
-            ignoreModalizer,
             ignoreGroupper,
             false
         );
@@ -1123,14 +1128,12 @@ export class FocusableAPI implements Types.FocusableAPI {
         current: HTMLElement,
         context?: HTMLElement,
         includeProgrammaticallyFocusable?: boolean,
-        ignoreModalizer?: boolean,
         ignoreGroupper?: boolean
     ): HTMLElement | null {
         return this._findElement(
             context || this._getBody(),
             current,
             includeProgrammaticallyFocusable,
-            ignoreModalizer,
             ignoreGroupper,
             true
         );
@@ -1139,14 +1142,12 @@ export class FocusableAPI implements Types.FocusableAPI {
     findDefault(
         context?: HTMLElement,
         includeProgrammaticallyFocusable?: boolean,
-        ignoreModalizer?: boolean,
         ignoreGroupper?: boolean
     ): HTMLElement | null {
         return this._findElement(
             context || this._getBody(),
             null,
             includeProgrammaticallyFocusable,
-            ignoreModalizer,
             ignoreGroupper,
             false,
             el => (this._tabster.focusable.isFocusable(el, includeProgrammaticallyFocusable) && !!this.getProps(el).isDefault)
@@ -1159,7 +1160,6 @@ export class FocusableAPI implements Types.FocusableAPI {
      * @param context @see {@link _findElement}
      * @param customFilter A callback that checks whether an element should be added to results
      * @param ignoreProgrammaticallyFocusable @see {@link _findElement}
-     * @param ignoreModalizer @see {@link _findElement}
      * @param ignoreGroupper @see {@link _findElement}
      * @param skipDefaultCondition skips the default condition that leverages @see {@link isFocusable}, be careful using this
      */
@@ -1167,7 +1167,6 @@ export class FocusableAPI implements Types.FocusableAPI {
         context: HTMLElement,
         customFilter: (el: HTMLElement) => boolean,
         includeProgrammaticallyFocusable?: boolean,
-        ignoreModalizer?: boolean,
         ignoreGroupper?: boolean,
         skipDefaultCondition?: boolean
     ): HTMLElement[] {
@@ -1192,7 +1191,6 @@ export class FocusableAPI implements Types.FocusableAPI {
                 this._acceptElement(
                     node as HTMLElement,
                     acceptCondition,
-                    ignoreModalizer,
                     ignoreGroupper
                 )
         );
@@ -1216,7 +1214,6 @@ export class FocusableAPI implements Types.FocusableAPI {
         container: HTMLElement | undefined,
         currentElement: HTMLElement | null,
         includeProgrammaticallyFocusable?: boolean,
-        ignoreModalizer?: boolean,
         ignoreGroupper?: boolean,
         prev?: boolean,
         acceptCondition?: (el: HTMLElement) => boolean
@@ -1236,7 +1233,7 @@ export class FocusableAPI implements Types.FocusableAPI {
         const walker = createElementTreeWalker(
             container.ownerDocument,
             container,
-            (node) => this._acceptElement(node as HTMLElement, acceptCondition!!!, ignoreModalizer, ignoreGroupper)
+            (node) => this._acceptElement(node as HTMLElement, acceptCondition!!!, ignoreGroupper)
         );
 
         if (!walker) {
@@ -1256,7 +1253,7 @@ export class FocusableAPI implements Types.FocusableAPI {
                 return null;
             }
 
-            if (this._acceptElement(lastChild, acceptCondition!!!, ignoreModalizer, ignoreGroupper) === NodeFilter.FILTER_ACCEPT) {
+            if (this._acceptElement(lastChild, acceptCondition!!!, ignoreGroupper) === NodeFilter.FILTER_ACCEPT) {
                 return lastChild;
             } else {
                 walker.currentNode = lastChild;
@@ -1269,24 +1266,22 @@ export class FocusableAPI implements Types.FocusableAPI {
     private _acceptElement(
         element: HTMLElement,
         acceptCondition: (el: HTMLElement) => boolean,
-        ignoreModalizer?: boolean,
         ignoreGroupper?: boolean
     ): number {
         const ctx = RootAPI.getTabsterContext(this._tabster, element);
-        const currentModalizerId = ctx && ctx.root.getCurrentModalizerId();
-
-        if (ignoreModalizer || (!ctx || !ctx.modalizer) ||
-            (currentModalizerId === undefined) ||
-            (currentModalizerId === ctx.modalizer.userId) ||
-            ctx.modalizer.getBasicProps().isAlwaysAccessible
-        ) {
-            if (!ignoreGroupper && (this._isInCurrentGroupper(element, true) === false)) {
-                return NodeFilter.FILTER_REJECT;
-            }
-
-            return acceptCondition(element) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
+        // Tabster is opt in, if it is not managed, don't try and get do anything special
+        if (!ctx) {
+            return NodeFilter.FILTER_SKIP;
+        }
+        
+        if (!this.isAccessible(element)) {
+            return NodeFilter.FILTER_REJECT;
         }
 
-        return ctx ? NodeFilter.FILTER_REJECT : NodeFilter.FILTER_SKIP;
+        if (ignoreGroupper && (this._isInCurrentGroupper(element, true) === false)) {
+            return NodeFilter.FILTER_REJECT;
+        }
+
+        return acceptCondition(element) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
     }
 }

core/src/Deloser.ts
@@ -108,25 +108,6 @@ class DeloserHistoryByRoot extends DeloserHistoryByRootBase<Types.Deloser, Delos
             }
         }
 
-        const getWindow = (this._tabster as unknown as Types.TabsterInternal).getWindow;
-        const root = RootAPI.getRootByUId(getWindow, this.rootUId);
-        const modalizers = root && root.getModalizers();
-
-        if (modalizers) {
-            // Nothing satisfactory in the focus history, each Modalizer has Deloser,
-            // let's try to find something under the same root.
-            for (let m of modalizers) {
-                const e = m.getElement();
-                const tabsterOnElement = e && getTabsterOnElement(this._tabster, e);
-                const deloser = tabsterOnElement && tabsterOnElement.deloser;
-                const deloserItem = deloser && new DeloserItem(this._tabster, deloser);
-
-                if (deloserItem && await deloserItem.focusAvailable()) {
-                    return true;
-                }
-            }
-        }
-
         return false;
     }
 
@@ -141,24 +122,6 @@ class DeloserHistoryByRoot extends DeloserHistoryByRootBase<Types.Deloser, Delos
 
             if (!skip && !resetQueue[i.uid]) {
                 resetQueue[i.uid] = i;
-            }
-        }
-
-        const getWindow = (this._tabster as unknown as Types.TabsterInternal).getWindow;
-        const root = RootAPI.getRootByUId(getWindow, this.rootUId);
-        const modalizers = root && root.getModalizers();
-
-        if (modalizers) {
-            // Nothing satisfactory in the focus history, each Modalizer has Deloser,
-            // let's try to find something under the same root.
-            for (let m of modalizers) {
-                const e = m.getElement();
-                const tabsterOnElement = e && getTabsterOnElement(this._tabster, e);
-                const deloser = tabsterOnElement && tabsterOnElement.deloser;
-
-                if (deloser && !(deloser.uid in resetQueue)) {
-                    resetQueue[deloser.uid] = new DeloserItem(this._tabster, deloser);
-                }
             }
         }
 
@@ -196,7 +159,7 @@ export class DeloserHistory {
 
         const historyByRoot = this.make(rootUId, () => new DeloserHistoryByRoot(this._tabster, rootUId));
 
-        if (!ctx || !ctx.modalizer || (ctx.root.getCurrentModalizerId() === ctx.modalizer.userId)) {
+        if (!ctx || !ctx.modalizer || ctx.modalizer?.isActive()) {
             historyByRoot.unshiftToDeloser(deloser, element);
         }
 

commit 5c0ea3c3fe73439b6dd3e8a73191d89e6632b85e

fix(Focusable): Groupper condition in _acceptElement was inverted
Fixes a bug introduced in #27 when the `_acceptElement` function was
modified

core/src/Focusable.ts
@@ -1278,7 +1278,7 @@ export class FocusableAPI implements Types.FocusableAPI {
             return NodeFilter.FILTER_REJECT;
         }
 
-        if (ignoreGroupper && (this._isInCurrentGroupper(element, true) === false)) {
+        if (!ignoreGroupper && (this._isInCurrentGroupper(element, true) === false)) {
             return NodeFilter.FILTER_REJECT;
         }
 