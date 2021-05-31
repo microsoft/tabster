fc28c2a3665720e23a3142a8b8e525f0f614f3dc
Marat Abdullin <marata@microsoft.com>
2021-05-31 17:06:09 +0200

    Conflicting values for 'process.env.NODE_ENV'.

--- test-container/webpack.config.js
+++ test-container/webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-                NODE_ENV: "development"
+                NODE_ENV: '"development"'
             },
             '__DEV__': true
         }),
