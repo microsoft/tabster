2a08f07573cc63fa02f5ba2c49445839b4d1612e
Marat Abdullin <marata@microsoft.com>
2021-07-13 14:39:26 +0200

    Adding a test.

--- core/src/__tests__/Uncontrolled.test.tsx
+++ core/src/__tests__/Uncontrolled.test.tsx
@@ -67,4 +67,63 @@ describe('Uncontrolled', () => {
                 expect(el?.textContent).toContain('Button4');
             });
     });
+
+    it('should allow to go outside of the application even if the uncontrolled element is the first/last in the application', async () => {
+        await new BroTest.BroTest(
+            (
+                <div {...getTabsterAttribute({ root: {} })}>
+                    <div {...getTabsterAttribute({ uncontrolled: {} })}>
+                        <button>Button1</button>
+                    </div>
+                    <button>Button2</button>
+                    <button>Button3</button>
+                    <div {...getTabsterAttribute({ uncontrolled: {} })}>
+                        <button>Button4</button>
+                    </div>
+                </div>
+            )
+        )
+            .pressTab()
+            .activeElement(el => {
+                expect(el?.textContent).toContain('Button1');
+            })
+            .pressTab()
+            .activeElement(el => {
+                expect(el?.textContent).toContain('Button2');
+            })
+            .pressTab()
+            .activeElement(el => {
+                expect(el?.textContent).toContain('Button3');
+            })
+            .pressTab()
+            .activeElement(el => {
+                expect(el?.textContent).toContain('Button4');
+            })
+            .pressTab()
+            .activeElement(el => {
+                // For some reason, the Puppeeteer testing environment doesn't
+                // move the focus to the address bar. In the real world, we should
+                // have undefined here, but the testing environment moves the focus
+                // to the first button.
+                expect(el?.textContent).toContain('Button1');
+            })
+            .pressTab()
+            .activeElement(el => {
+                expect(el?.textContent).toContain('Button2');
+            })
+            .pressTab(true)
+            .activeElement(el => {
+                expect(el?.textContent).toContain('Button1');
+            })
+            .pressTab(true)
+            .activeElement(el => {
+                // Tabbing backwards in the testing environment however does move
+                // the focus outside of the app.
+                expect(el?.textContent).toBeUndefined();
+            })
+            .pressTab()
+            .activeElement(el => {
+                expect(el?.textContent).toContain('Button1');
+            });
+    });
 });
