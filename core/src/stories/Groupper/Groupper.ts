0b9f63d864edd920b3f381129d1a44c72330e273
ling1726 <lingfan.gao@microsoft.com>
2021-11-19 16:59:28 +0100

    chore(storybook): Adds @storybook/html to `core` (#76)
    * chore(storybook): Adds @storybook/html to `core`
    
    Setup a storybook in `core` for the removal of `examples` and
    `demo` packages in a follow up PR.
    
    The stories include
    
    * Mover
    * Modalizer
    * Groupper
    
    * tidy up modal dialog
    
    * restore prepare

---
+++ core/src/stories/Groupper/Groupper.ts
@@ -0,0 +1,32 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import './groupper.css';
+import { getTabsterAttribute, Types as TabsterTypes } from '../..';
+
+export interface FocusableContainerProps extends TabsterTypes.GroupperProps {}
+
+export const createFocusableContainer = (props: FocusableContainerProps) => {
+    const { tabbability } = props;
+
+    const wrapper = document.createElement('div');
+    wrapper.tabIndex = 0;
+    wrapper.classList.add('item');
+
+    const attr = getTabsterAttribute({
+        groupper: {
+            tabbability
+        },
+    }, true);
+
+    wrapper.setAttribute(TabsterTypes.TabsterAttributeName, attr);
+
+    wrapper.innerHTML = `
+    <button>Focusable button</button>
+    <button>Focusable button</button>
+  `;
+
+    return wrapper;
+};
