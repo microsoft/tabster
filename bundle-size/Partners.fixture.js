e838604ba87d9aa874ca0b465519fd61a8798927
Bernardo Sunderhus <bsunderhus@microsoft.com>
2021-11-24 11:14:49 +0100

    Adds partners fixture

--- bundle-size/Partners.fixture.js
+++ bundle-size/Partners.fixture.js
@@ -1,45 +1,29 @@
 import {
     createTabster,
     disposeTabster,
-    forceCleanup,
     getTabsterAttribute,
     setTabsterAttribute,
     getGroupper,
     getMover,
-    getCrossOrigin,
     getDeloser,
     getModalizer,
-    getObservedElement,
-    getOutline,
     getCurrentTabster,
-    getInternal,
-    overrideBasics,
-    makeNoOp,
-    isNoOp,
     Types,
 } from "tabster";
 
 console.log(
     createTabster,
     disposeTabster,
-    forceCleanup,
     getTabsterAttribute,
     setTabsterAttribute,
     getGroupper,
     getMover,
-    getCrossOrigin,
     getDeloser,
     getModalizer,
-    getObservedElement,
-    getOutline,
     getCurrentTabster,
-    getInternal,
-    overrideBasics,
-    makeNoOp,
-    isNoOp,
     Types
 );
 
 export default {
-    name: "All Tabster",
+    name: "Partners import",
 };
