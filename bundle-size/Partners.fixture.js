80535e1e11a13d55f40c6de34abaffd4efe52889
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2021-11-24 11:25:18 +0100

    Adds bundle-size tooling (#84)
    * Adds bundle-size tooling
    
    * Adds partners fixture

---
+++ bundle-size/Partners.fixture.js
@@ -0,0 +1,29 @@
+import {
+    createTabster,
+    disposeTabster,
+    getTabsterAttribute,
+    setTabsterAttribute,
+    getGroupper,
+    getMover,
+    getDeloser,
+    getModalizer,
+    getCurrentTabster,
+    Types,
+} from "tabster";
+
+console.log(
+    createTabster,
+    disposeTabster,
+    getTabsterAttribute,
+    setTabsterAttribute,
+    getGroupper,
+    getMover,
+    getDeloser,
+    getModalizer,
+    getCurrentTabster,
+    Types
+);
+
+export default {
+    name: "Partners import",
+};
