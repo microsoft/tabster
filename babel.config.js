99bf238513a4920b9dfa1810f127e2b1a54e35c1
Lingfan Gao <lingfangao@hotmail.com>
2021-11-24 09:38:26 +0100

    chore: Replace `ts-jest` with `babel-jest`
    Replaces `ts-jest` with `babel-jest` as the test code transformer.
    
    Changes `babel.config.json` with `babel.config.js` to support more
    environments in the future as we move away from tsdx

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+module.exports = (api) => {
+    const isTest = api.env('test');
+    
+    // Currently babel is only used to transform tests
+    if (!isTest) {
+      return {};
+    }
+
+    return {
+        presets: [
+            "@babel/preset-react",
+            "@babel/preset-typescript",
+             ["@babel/preset-env", { targets: { node: "current" } }],
+        ],
+        plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-proposal-optional-chaining",
+            "@babel/plugin-transform-typescript",
+            [
+                "@babel/plugin-transform-react-jsx",
+                { pragma: "BroTest.createElementString" },
+            ],
+        ],
+    };
+};
