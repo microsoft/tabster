3615207d833fab2b3e55fbbf278f108763731c6d
Lingfan Gao <lingfangao@hotmail.com>
2021-11-26 19:50:53 +0100

    IE11 EOL

--- babel.config.js
+++ babel.config.js
@@ -8,7 +8,6 @@ module.exports = (api) => {
               {
                   modules: false,
                   loose: true,
-                  forceAllTransforms: true,
               },
           ];
 
