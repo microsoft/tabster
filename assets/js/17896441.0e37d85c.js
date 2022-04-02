1b140c8e52922eee0f59180608278df7b29998de
mshoho <marata@microsoft.com>
2022-04-02 18:55:18 +0000

    Deploying to gh-pages from @ microsoft/tabster@d3bd4a576b619823eaf14b0dd649080e98a1fd91 🚀

---
+++ assets/js/17896441.0e37d85c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[918],{6922:function(e,t,o){var r=o(3117),s=o(7294),c=Object.assign({React:s},s,{ButtonExample:function(e){return s.createElement("button",(0,r.Z)({},e,{style:Object.assign({backgroundColor:"white",color:"black",border:"solid red",borderRadius:20,padding:10,cursor:"pointer"},e.style)}))}});t.Z=c}}]);
