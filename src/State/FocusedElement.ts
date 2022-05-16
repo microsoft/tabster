fbe83abae9513cb03a810f8d25147587e21b503d
ling1726 <lingfangao@hotmail.com>
2022-05-16 16:41:33 +0200

    feat: Allow circular focus trap in Modalizer (#146)
    * feat: Allow circular focus trap in Modalizer
    
    Implements a new prop for Modalizer `circular` which allows focus to
    stay within the Modalizer and never leave the it. The prop is
    implemented for both controlled an uncontrolled code paths in Tabster.
    
    * fix type
    
    * change circular to isTrapped

--- src/State/FocusedElement.ts
+++ src/State/FocusedElement.ts
@@ -460,7 +460,7 @@ export class FocusedElementState
             return;
         }
 
-        const nextElement = next.element;
+        let nextElement = next.element;
 
         if (ctx.modalizer) {
             const nextElementCtx =
@@ -477,6 +477,18 @@ export class FocusedElementState
 
                     return;
                 }
+            }
+
+            // circular focus trap for modalizer
+            if (
+                !nextElement &&
+                ctx.modalizer.isActive() &&
+                ctx.modalizer.getProps().isTrapped
+            ) {
+                const findFn = isPrev ? "findLast" : "findFirst";
+                nextElement = this._tabster.focusable[findFn]({
+                    container: ctx.modalizer.getElement(),
+                });
             }
         }
 
