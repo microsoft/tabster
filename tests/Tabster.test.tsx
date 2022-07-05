microsoft/tabster

commit 8648e07a8f32f4d3fbe90fdf2c511db33cb66b68

Create lightweight Tabster instance backed by a global TabsterCore (#136)

src/Tabster.ts
@@ -30,15 +30,38 @@ import {
 
 export { Types };
 
+class Tabster implements Types.Tabster {
+    keyboardNavigation: Types.KeyboardNavigationState;
+    focusedElement: Types.FocusedElementState;
+    focusable: Types.FocusableAPI;
+    root: Types.RootAPI;
+    uncontrolled: Types.UncontrolledAPI;
+    core: Types.TabsterCore;
+
+    constructor(tabster: Types.TabsterCore) {
+        this.keyboardNavigation = tabster.keyboardNavigation;
+        this.focusedElement = tabster.focusedElement;
+        this.focusable = tabster.focusable;
+        this.root = tabster.root;
+        this.uncontrolled = tabster.uncontrolled;
+        this.core = tabster;
+    }
+
+    dispose() {
+        this.core.disposeTabster(this);
+    }
+}
+
 /**
  * Extends Window to include an internal Tabster instance.
  */
-class Tabster implements Types.TabsterCore {
+class TabsterCore implements Types.TabsterCore {
     private _storage: WeakMap<HTMLElement, Types.TabsterElementStorage>;
     private _unobserve: (() => void) | undefined;
     private _win: WindowWithTabsterInstance | undefined;
     private _forgetMemorizedTimer: number | undefined;
     private _forgetMemorizedElements: HTMLElement[] = [];
+    private _wrappers: Set<Tabster> = new Set<Tabster>();
 
     _version: string = __VERSION__;
     _noop = false;
@@ -102,6 +125,20 @@ class Tabster implements Types.TabsterCore {
         startFakeWeakRefsCleanup(getWindow);
     }
 
+    createTabster(): Types.Tabster {
+        const wrapper = new Tabster(this);
+        this._wrappers.add(wrapper);
+        return wrapper;
+    }
+
+    disposeTabster(wrapper: Types.Tabster) {
+        this._wrappers.delete(wrapper);
+
+        if (this._wrappers.size === 0) {
+            this.dispose();
+        }
+    }
+
     dispose(): void {
         this.internal.stopObserver();
 
@@ -131,6 +168,7 @@ class Tabster implements Types.TabsterCore {
         clearElementCache(this.getWindow);
 
         this._storage = new WeakMap();
+        this._wrappers.clear();
 
         if (win) {
             disposeInstanceContext(win);
@@ -166,39 +204,40 @@ class Tabster implements Types.TabsterCore {
         return this._win;
     };
 
-    static forceCleanup(tabster: Tabster): void {
-        if (!tabster._win) {
+    forceCleanup(): void {
+        if (!this._win) {
             return;
         }
 
-        tabster._forgetMemorizedElements.push(tabster._win.document.body);
+        this._forgetMemorizedElements.push(this._win.document.body);
 
-        if (tabster._forgetMemorizedTimer) {
+        if (this._forgetMemorizedTimer) {
             return;
         }
 
-        tabster._forgetMemorizedTimer = tabster._win.setTimeout(() => {
-            delete tabster._forgetMemorizedTimer;
+        this._forgetMemorizedTimer = this._win.setTimeout(() => {
+            delete this._forgetMemorizedTimer;
 
             for (
                 let el: HTMLElement | undefined =
-                    tabster._forgetMemorizedElements.shift();
+                    this._forgetMemorizedElements.shift();
                 el;
-                el = tabster._forgetMemorizedElements.shift()
+                el = this._forgetMemorizedElements.shift()
             ) {
-                clearElementCache(tabster.getWindow, el);
-                FocusedElementState.forgetMemorized(tabster.focusedElement, el);
+                clearElementCache(this.getWindow, el);
+                FocusedElementState.forgetMemorized(this.focusedElement, el);
             }
         }, 0);
 
-        cleanupFakeWeakRefs(tabster.getWindow, true);
+        cleanupFakeWeakRefs(this.getWindow, true);
     }
 }
 
-export function forceCleanup(tabster: Tabster): void {
+export function forceCleanup(tabster: Types.Tabster): void {
     // The only legit case for calling this method is when you've completely removed
     // the application DOM and not going to add the new one for a while.
-    Tabster.forceCleanup(tabster);
+    const tabsterCore = tabster.core;
+    tabsterCore.forceCleanup();
 }
 
 /**
@@ -207,55 +246,60 @@ export function forceCleanup(tabster: Tabster): void {
 export function createTabster(
     win: Window,
     props?: Types.TabsterCoreProps
-): Types.TabsterCore {
-    const existingAh = getCurrentTabster(win as WindowWithTabsterInstance);
+): Types.Tabster {
+    let tabster = getCurrentTabster(win as WindowWithTabsterInstance);
 
-    if (existingAh) {
+    if (tabster) {
         if (__DEV__) {
             console.warn(
                 "Attempted to create a duplicate Tabster instance on the window"
             );
         }
 
-        return existingAh;
+        return tabster.createTabster();
     }
 
-    const tabster = new Tabster(win, props);
+    tabster = new TabsterCore(win, props);
     (win as WindowWithTabsterInstance).__tabsterInstance = tabster;
-
-    return tabster;
+    return tabster.createTabster();
 }
 
 /**
  * Creates a new groupper instance or returns an existing one
  * @param tabster Tabster instance
  */
-export function getGroupper(tabster: Types.TabsterCore): Types.GroupperAPI {
-    if (!tabster.groupper) {
-        tabster.groupper = new GroupperAPI(tabster, tabster.getWindow);
+export function getGroupper(tabster: Types.Tabster): Types.GroupperAPI {
+    const tabsterCore = tabster.core;
+    if (!tabsterCore.groupper) {
+        tabsterCore.groupper = new GroupperAPI(
+            tabsterCore,
+            tabsterCore.getWindow
+        );
     }
 
-    return tabster.groupper;
+    return tabsterCore.groupper;
 }
 
 /**
  * Creates a new mover instance or returns an existing one
  * @param tabster Tabster instance
  */
-export function getMover(tabster: Types.TabsterCore): Types.MoverAPI {
-    if (!tabster.mover) {
-        tabster.mover = new MoverAPI(tabster, tabster.getWindow);
+export function getMover(tabster: Types.Tabster): Types.MoverAPI {
+    const tabsterCore = tabster.core;
+    if (!tabsterCore.mover) {
+        tabsterCore.mover = new MoverAPI(tabsterCore, tabsterCore.getWindow);
     }
 
-    return tabster.mover;
+    return tabsterCore.mover;
 }
 
-export function getOutline(tabster: Types.TabsterCore): Types.OutlineAPI {
-    if (!tabster.outline) {
-        tabster.outline = new OutlineAPI(tabster);
+export function getOutline(tabster: Types.Tabster): Types.OutlineAPI {
+    const tabsterCore = tabster.core;
+    if (!tabsterCore.outline) {
+        tabsterCore.outline = new OutlineAPI(tabsterCore);
     }
 
-    return tabster.outline;
+    return tabsterCore.outline;
 }
 
 /**
@@ -264,59 +308,62 @@ export function getOutline(tabster: Types.TabsterCore): Types.OutlineAPI {
  * @param props Deloser props
  */
 export function getDeloser(
-    tabster: Types.TabsterCore,
+    tabster: Types.Tabster,
     props?: { autoDeloser: Types.DeloserProps }
 ): Types.DeloserAPI {
-    if (!tabster.deloser) {
-        tabster.deloser = new DeloserAPI(tabster, props);
+    const tabsterCore = tabster.core;
+    if (!tabsterCore.deloser) {
+        tabsterCore.deloser = new DeloserAPI(tabsterCore, props);
     }
 
-    return tabster.deloser;
+    return tabsterCore.deloser;
 }
 
 /**
  * Creates a new modalizer instance or returns an existing one
  * @param tabster Tabster instance
  */
-export function getModalizer(tabster: Types.TabsterCore): Types.ModalizerAPI {
-    if (!tabster.modalizer) {
-        tabster.modalizer = new ModalizerAPI(tabster);
+export function getModalizer(tabster: Types.Tabster): Types.ModalizerAPI {
+    const tabsterCore = tabster.core;
+    if (!tabsterCore.modalizer) {
+        tabsterCore.modalizer = new ModalizerAPI(tabsterCore);
     }
 
-    return tabster.modalizer;
+    return tabsterCore.modalizer;
 }
 
 export function getObservedElement(
-    tabster: Types.TabsterCore
+    tabster: Types.Tabster
 ): Types.ObservedElementAPI {
-    if (!tabster.observedElement) {
-        tabster.observedElement = new ObservedElementAPI(tabster);
+    const tabsterCore = tabster.core;
+    if (!tabsterCore.observedElement) {
+        tabsterCore.observedElement = new ObservedElementAPI(tabsterCore);
     }
 
-    return tabster.observedElement;
+    return tabsterCore.observedElement;
 }
 
-export function getCrossOrigin(
-    tabster: Types.TabsterCore
-): Types.CrossOriginAPI {
-    if (!tabster.crossOrigin) {
+export function getCrossOrigin(tabster: Types.Tabster): Types.CrossOriginAPI {
+    const tabsterCore = tabster.core;
+    if (!tabsterCore.crossOrigin) {
         getDeloser(tabster);
         getModalizer(tabster);
         getMover(tabster);
         getGroupper(tabster);
         getOutline(tabster);
         getObservedElement(tabster);
-        tabster.crossOrigin = new CrossOriginAPI(tabster);
+        tabsterCore.crossOrigin = new CrossOriginAPI(tabsterCore);
     }
 
-    return tabster.crossOrigin;
+    return tabsterCore.crossOrigin;
 }
 
-export function getInternal(tabster: Types.TabsterCore): Types.InternalAPI {
-    return tabster.internal;
+export function getInternal(tabster: Types.Tabster): Types.InternalAPI {
+    const tabsterCore = tabster.core;
+    return tabsterCore.internal;
 }
 
-export function disposeTabster(tabster: Types.TabsterCore): void {
+export function disposeTabster(tabster: Types.Tabster): void {
     tabster.dispose();
 }
 
@@ -405,7 +452,7 @@ export function getCurrentTabster(win: Window): Types.TabsterCore | undefined {
 }
 
 export function makeNoOp(tabster: Types.TabsterCore, noop: boolean): void {
-    const self = tabster as Tabster;
+    const self = tabster as TabsterCore;
 
     if (self._noop !== noop) {
         self._noop = noop;
@@ -441,5 +488,5 @@ export function makeNoOp(tabster: Types.TabsterCore, noop: boolean): void {
 }
 
 export function isNoOp(tabster: Types.TabsterCore): boolean {
-    return (tabster as Tabster)._noop;
+    return (tabster as TabsterCore)._noop;
 }

tests/Tabster.test.tsx
@@ -1,0 +1,62 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import * as React from "react";
+import * as BroTest from "./utils/BroTest";
+
+interface WindowWithTabster extends Window {
+    __tabsterInstance: unknown;
+}
+
+describe("Tabster dispose", () => {
+    beforeEach(async () => {
+        await BroTest.bootstrapTabsterPage({});
+    });
+
+    it("should not dispose global tabster core if there are still tabster instances", async () => {
+        await new BroTest.BroTest(<div />)
+            .eval(() => {
+                // dispose default tabster on the test page
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                window.__tabsterInstance.dispose();
+                const first = getTabsterTestVariables().createTabster?.(window);
+                getTabsterTestVariables().createTabster?.(window);
+
+                if (first) {
+                    getTabsterTestVariables().disposeTabster?.(first);
+                }
+
+                return !!(window as unknown as WindowWithTabster)
+                    .__tabsterInstance;
+            })
+            .check((tabsterExists) => {
+                expect(tabsterExists).toBe(true);
+            });
+    });
+
+    it("should dispose global tabster core if there are no more tabster instances", async () => {
+        await new BroTest.BroTest(<div />)
+            .eval(() => {
+                // dispose default tabster on the test page
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                window.__tabsterInstance.dispose();
+                const first = getTabsterTestVariables().createTabster?.(window);
+                const second =
+                    getTabsterTestVariables().createTabster?.(window);
+
+                if (first && second) {
+                    getTabsterTestVariables().disposeTabster?.(first);
+                    getTabsterTestVariables().disposeTabster?.(second);
+                }
+                return !!(window as unknown as WindowWithTabster)
+                    .__tabsterInstance;
+            })
+            .check((tabsterExists) => {
+                expect(tabsterExists).toBe(false);
+            });
+    });
+});

tests/index.html
@@ -4,9 +4,9 @@
         <title>Tabster Test</title>
         <script type="module">
             import {
+                disposeTabster,
                 createTabster,
                 getCrossOrigin,
-                getCurrentTabster,
                 getDeloser,
                 getGroupper,
                 getModalizer,
@@ -15,9 +15,7 @@
                 getOutline,
             } from "../src";
 
-            const tabsterTest = {
-                getCurrentTabster,
-            };
+            const tabsterTest = {};
 
             const params = new URL(location.href).searchParams;
             const controlTab = params.get("controlTab") !== "false";
@@ -34,6 +32,9 @@
                 controlTab,
                 rootDummyInputs,
             });
+
+            tabsterTest.createTabster = createTabster;
+            tabsterTest.disposeTabster = disposeTabster;
 
             tabsterTest.core = tabster;
 

tests/utils/BroTest.ts
@@ -11,7 +11,7 @@ import {
     Frame,
     KeyInput,
 } from "puppeteer";
-import { getCurrentTabster, Types } from "tabster";
+import { createTabster, disposeTabster, Types } from "tabster";
 
 // Importing the production version so that React doesn't complain in the test output.
 declare function require(name: string): any;
@@ -85,8 +85,9 @@ async function waitPageReadyAndDecorateConsoleError(
 }
 
 export interface BroTestTabsterTestVariables {
-    getCurrentTabster: typeof getCurrentTabster;
-    core?: Types.TabsterCore;
+    disposeTabster?: typeof disposeTabster;
+    createTabster?: typeof createTabster;
+    core?: Types.Tabster;
     modalizer?: Types.ModalizerAPI;
     deloser?: Types.DeloserAPI;
     outline?: Types.OutlineAPI;

.storybook/preview.js
@@ -1,6 +1,6 @@
 import {
     createTabster,
-    getCurrentTabster,
+    disposeTabster,
     getDeloser,
     getGroupper,
     getModalizer,
@@ -22,36 +22,37 @@ export const parameters = {
 
 export const decorators = [
     (Story) => {
-        if (!getCurrentTabster(window)) {
-            const controlTab = !process.env.STORYBOOK_UNCONTROLLED;
-            const rootDummyInputs = process.env.STORYBOOK_ROOT_DUMMY_INPUTS;
-            const tabster = createTabster(window, {
-                autoRoot: {},
-                controlTab,
-                rootDummyInputs,
-            });
-            console.log(
-                "created tabster",
-                `as ${
-                    controlTab ? "controlled" : "uncontrolled"
-                }, root dummy inputs ${rootDummyInputs}`
-            );
-            getModalizer(tabster);
-            console.log("created modalizer");
-            getDeloser(tabster);
-            console.log("created deloser");
-            getOutline(tabster);
-            console.log("created outline");
-            getMover(tabster);
-            console.log("created mover");
-            getGroupper(tabster);
-            console.log("created groupper");
-            getObservedElement(tabster);
-            console.log("created observed");
-            getCrossOrigin(tabster);
-            tabster?.crossOrigin?.setup();
-            console.log("created cross origin");
-        }
+        const controlTab = !process.env.STORYBOOK_UNCONTROLLED;
+        const rootDummyInputs = process.env.STORYBOOK_ROOT_DUMMY_INPUTS;
+        const tabster = createTabster(window, {
+            autoRoot: {},
+            controlTab,
+            rootDummyInputs,
+        });
+        console.log(
+            "created tabster",
+            `as ${
+                controlTab ? "controlled" : "uncontrolled"
+            }, root dummy inputs ${rootDummyInputs}`
+        );
+        getModalizer(tabster);
+        console.log("created modalizer");
+        getDeloser(tabster);
+        console.log("created deloser");
+        getOutline(tabster);
+        console.log("created outline");
+        getMover(tabster);
+        console.log("created mover");
+        getGroupper(tabster);
+        console.log("created groupper");
+        getObservedElement(tabster);
+        console.log("created observed");
+        const crossOrigin = getCrossOrigin(tabster);
+        crossOrigin.setup();
+        console.log("created cross origin");
+
+        window.createTabster = createTabster;
+        window.disposeTabster = disposeTabster;
 
         return Story();
     },

bundle-size/Deloser.fixture.js
@@ -4,7 +4,6 @@ import {
     getTabsterAttribute,
     setTabsterAttribute,
     getDeloser,
-    getCurrentTabster,
     Types,
 } from "tabster";
 
@@ -14,7 +13,6 @@ console.log(
     getTabsterAttribute,
     setTabsterAttribute,
     getDeloser,
-    getCurrentTabster,
     Types
 );
 

bundle-size/Groupper.fixture.js
@@ -4,7 +4,6 @@ import {
     getTabsterAttribute,
     setTabsterAttribute,
     getGroupper,
-    getCurrentTabster,
     Types,
 } from "tabster";
 
@@ -14,7 +13,6 @@ console.log(
     getTabsterAttribute,
     setTabsterAttribute,
     getGroupper,
-    getCurrentTabster,
     Types
 );
 

bundle-size/Modalizer.fixture.js
@@ -4,7 +4,6 @@ import {
     getTabsterAttribute,
     setTabsterAttribute,
     getModalizer,
-    getCurrentTabster,
     Types,
 } from "tabster";
 
@@ -14,7 +13,6 @@ console.log(
     getTabsterAttribute,
     setTabsterAttribute,
     getModalizer,
-    getCurrentTabster,
     Types
 );
 

commit 1bbcaf2fb18682a6e6d9fce5954b2548b327d774

fix: get<API> should accept both Tabster and TabsterCore instance
Fixes backwards compat issue of Tabster/TabsterCore instances introduced
with #136

src/Tabster.ts
@@ -51,7 +51,7 @@ class Tabster implements Types.Tabster {
 /**
  * Extends Window to include an internal Tabster instance.
  */
-class TabsterCore implements Types.TabsterCore {
+export class TabsterCore implements Types.TabsterCore {
     private _storage: WeakMap<HTMLElement, Types.TabsterElementStorage>;
     private _unobserve: (() => void) | undefined;
     private _win: WindowWithTabsterInstance | undefined;
@@ -262,8 +262,10 @@ export function createTabster(
  * Creates a new groupper instance or returns an existing one
  * @param tabster Tabster instance
  */
-export function getGroupper(tabster: Types.Tabster): Types.GroupperAPI {
-    const tabsterCore = tabster.core;
+export function getGroupper(
+    tabster: Types.Tabster | TabsterCore
+): Types.GroupperAPI {
+    const tabsterCore = tabster instanceof TabsterCore ? tabster : tabster.core;
     if (!tabsterCore.groupper) {
         tabsterCore.groupper = new GroupperAPI(
             tabsterCore,
@@ -278,8 +280,8 @@ export function getGroupper(tabster: Types.Tabster): Types.GroupperAPI {
  * Creates a new mover instance or returns an existing one
  * @param tabster Tabster instance
  */
-export function getMover(tabster: Types.Tabster): Types.MoverAPI {
-    const tabsterCore = tabster.core;
+export function getMover(tabster: Types.Tabster | TabsterCore): Types.MoverAPI {
+    const tabsterCore = tabster instanceof TabsterCore ? tabster : tabster.core;
     if (!tabsterCore.mover) {
         tabsterCore.mover = new MoverAPI(tabsterCore, tabsterCore.getWindow);
     }
@@ -287,8 +289,10 @@ export function getMover(tabster: Types.Tabster): Types.MoverAPI {
     return tabsterCore.mover;
 }
 
-export function getOutline(tabster: Types.Tabster): Types.OutlineAPI {
-    const tabsterCore = tabster.core;
+export function getOutline(
+    tabster: Types.Tabster | TabsterCore
+): Types.OutlineAPI {
+    const tabsterCore = tabster instanceof TabsterCore ? tabster : tabster.core;
     if (!tabsterCore.outline) {
         tabsterCore.outline = new OutlineAPI(tabsterCore);
     }
@@ -302,10 +306,10 @@ export function getOutline(tabster: Types.Tabster): Types.OutlineAPI {
  * @param props Deloser props
  */
 export function getDeloser(
-    tabster: Types.Tabster,
+    tabster: Types.Tabster | TabsterCore,
     props?: { autoDeloser: Types.DeloserProps }
 ): Types.DeloserAPI {
-    const tabsterCore = tabster.core;
+    const tabsterCore = tabster instanceof TabsterCore ? tabster : tabster.core;
     if (!tabsterCore.deloser) {
         tabsterCore.deloser = new DeloserAPI(tabsterCore, props);
     }
@@ -317,8 +321,10 @@ export function getDeloser(
  * Creates a new modalizer instance or returns an existing one
  * @param tabster Tabster instance
  */
-export function getModalizer(tabster: Types.Tabster): Types.ModalizerAPI {
-    const tabsterCore = tabster.core;
+export function getModalizer(
+    tabster: Types.Tabster | TabsterCore
+): Types.ModalizerAPI {
+    const tabsterCore = tabster instanceof TabsterCore ? tabster : tabster.core;
     if (!tabsterCore.modalizer) {
         tabsterCore.modalizer = new ModalizerAPI(tabsterCore);
     }
@@ -327,9 +333,9 @@ export function getModalizer(tabster: Types.Tabster): Types.ModalizerAPI {
 }
 
 export function getObservedElement(
-    tabster: Types.Tabster
+    tabster: Types.Tabster | TabsterCore
 ): Types.ObservedElementAPI {
-    const tabsterCore = tabster.core;
+    const tabsterCore = tabster instanceof TabsterCore ? tabster : tabster.core;
     if (!tabsterCore.observedElement) {
         tabsterCore.observedElement = new ObservedElementAPI(tabsterCore);
     }
@@ -337,8 +343,11 @@ export function getObservedElement(
     return tabsterCore.observedElement;
 }
 
-export function getCrossOrigin(tabster: Types.Tabster): Types.CrossOriginAPI {
-    const tabsterCore = tabster.core;
+export function getCrossOrigin(
+    tabster: Types.Tabster | TabsterCore
+): Types.CrossOriginAPI {
+    const tabsterCore = tabster instanceof TabsterCore ? tabster : tabster.core;
+
     if (!tabsterCore.crossOrigin) {
         getDeloser(tabster);
         getModalizer(tabster);
@@ -352,8 +361,10 @@ export function getCrossOrigin(tabster: Types.Tabster): Types.CrossOriginAPI {
     return tabsterCore.crossOrigin;
 }
 
-export function getInternal(tabster: Types.Tabster): Types.InternalAPI {
-    const tabsterCore = tabster.core;
+export function getInternal(
+    tabster: Types.Tabster | TabsterCore
+): Types.InternalAPI {
+    const tabsterCore = tabster instanceof TabsterCore ? tabster : tabster.core;
     return tabsterCore.internal;
 }
 

tests/Tabster.test.tsx
@@ -4,11 +4,42 @@
  */
 
 import * as React from "react";
+import { TabsterCore } from "src/Tabster";
 import * as BroTest from "./utils/BroTest";
 
 interface WindowWithTabster extends Window {
     __tabsterInstance: unknown;
 }
+
+describe("getAPI", () => {
+    beforeEach(async () => {
+        await BroTest.bootstrapTabsterPage({});
+    });
+    it("should accept both Tabster and TabsterCore instances", async () => {
+        await new BroTest.BroTest(<div />)
+            .eval(() => {
+                // dispose default tabster on the test page
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                window.__tabsterInstance.dispose();
+                getTabsterTestVariables().createTabster?.(window);
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                const tabsterCore: TabsterCore = window.__tabsterInstance;
+
+                getTabsterTestVariables().getModalizer(tabsterCore);
+                getTabsterTestVariables().getCrossOrigin(tabsterCore);
+                getTabsterTestVariables().getDeloser(tabsterCore);
+                getTabsterTestVariables().getMover(tabsterCore);
+                getTabsterTestVariables().getObservedElement(tabsterCore);
+                getTabsterTestVariables().getOutline(tabsterCore);
+                getTabsterTestVariables().getGroupper(tabsterCore);
+
+                return true;
+            })
+            .check((success) => expect(success).toBe(true));
+    });
+});
 
 describe("Tabster dispose", () => {
     beforeEach(async () => {

tests/index.html
@@ -15,7 +15,15 @@
                 getOutline,
             } from "../src";
 
-            const tabsterTest = {};
+            const tabsterTest = {
+                getCrossOrigin,
+                getDeloser,
+                getGroupper,
+                getModalizer,
+                getMover,
+                getObservedElement,
+                getOutline,
+            };
 
             const params = new URL(location.href).searchParams;
             const controlTab = params.get("controlTab") !== "false";

tests/utils/BroTest.ts
@@ -11,7 +11,18 @@ import {
     Frame,
     KeyInput,
 } from "puppeteer";
-import { createTabster, disposeTabster, Types } from "tabster";
+import {
+    createTabster,
+    disposeTabster,
+    Types,
+    getMover as getMoverBase,
+    getCrossOrigin as getCrossOriginBase,
+    getDeloser as getDeloserBase,
+    getModalizer as getModalizerBase,
+    getOutline as getOutlineBase,
+    getObservedElement as getObservedElementBase,
+    getGroupper as getGroupperBase,
+} from "tabster";
 
 // Importing the production version so that React doesn't complain in the test output.
 declare function require(name: string): any;
@@ -95,6 +106,14 @@ export interface BroTestTabsterTestVariables {
     groupper?: Types.GroupperAPI;
     observedElement?: Types.ObservedElementAPI;
     crossOrigin?: Types.CrossOriginAPI;
+
+    getMover: typeof getMoverBase;
+    getGroupper: typeof getGroupperBase;
+    getModalizer: typeof getModalizerBase;
+    getObservedElement: typeof getObservedElementBase;
+    getOutline: typeof getOutlineBase;
+    getDeloser: typeof getDeloserBase;
+    getCrossOrigin: typeof getCrossOriginBase;
 }
 
 export function getTestPageURL(parts: TabsterParts): string {