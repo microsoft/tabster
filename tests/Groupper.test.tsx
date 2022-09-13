9fcc0ae8f8a4f6edf440a5dad6fa676eb9d43e75
Marat Abdullin <marata@microsoft.com>
2022-09-13 09:43:28 +0200

    feat(Groupper): Ability to delegate the role of focusable groupper container to first focusble element in groupper. (#189)

--- tests/Groupper.test.tsx
+++ tests/Groupper.test.tsx
@@ -11,13 +11,18 @@ import { runIfUnControlled } from "./utils/test-utils";
 const groupperItem = (
     tagName: "div" | "li",
     tabsterAttr: Types.TabsterDOMAttribute,
+    isFocusable: boolean,
     count: number
 ) => {
     const Tag = tagName;
     return (
-        <Tag tabIndex={0} {...tabsterAttr} data-count={`${count}`}>
-            <button>Foo</button>
-            <button>Bar</button>
+        <Tag
+            tabIndex={isFocusable ? 0 : undefined}
+            {...tabsterAttr}
+            data-count={`${count}`}
+        >
+            <button>Foo{count}</button>
+            <button>Bar{count}</button>
         </Tag>
     );
 };
@@ -58,10 +63,10 @@ describe("Groupper - default", () => {
 
         return (
             <div {...rootAttr}>
-                {groupperItem(tagName, groupperAttr, 1)}
-                {groupperItem(tagName, groupperAttr, 2)}
-                {groupperItem(tagName, groupperAttr, 3)}
-                {groupperItem(tagName, groupperAttr, 4)}
+                {groupperItem(tagName, groupperAttr, true, 1)}
+                {groupperItem(tagName, groupperAttr, true, 2)}
+                {groupperItem(tagName, groupperAttr, true, 3)}
+                {groupperItem(tagName, groupperAttr, true, 4)}
             </div>
         );
     };
@@ -83,7 +88,7 @@ describe("Groupper - default", () => {
             await new BroTest.BroTest(getTestHtml(tagName))
                 .pressTab()
                 .pressTab()
-                .activeElement((el) => expect(el?.textContent).toBe("Foo"));
+                .activeElement((el) => expect(el?.textContent).toBe("Foo1"));
         }
     );
 
@@ -108,7 +113,7 @@ describe("Groupper - default", () => {
                 })
                 .pressTab()
                 .pressTab()
-                .activeElement((el) => expect(el?.textContent).toEqual("Foo"))
+                .activeElement((el) => expect(el?.textContent).toEqual("Foo1"))
                 .pressEsc()
                 .activeElement((el) =>
                     expect(el?.attributes["data-count"]).toBe("1")
@@ -141,9 +146,9 @@ describe("Groupper - default", () => {
                 })
                 .pressTab()
                 .pressTab()
-                .activeElement((el) => expect(el?.textContent).toEqual("Foo"))
+                .activeElement((el) => expect(el?.textContent).toEqual("Foo1"))
                 .pressEsc()
-                .activeElement((el) => expect(el?.textContent).toEqual("Foo"))
+                .activeElement((el) => expect(el?.textContent).toEqual("Foo1"))
                 .eval(() => (window as WindowWithEscFlag).__escPressed2)
                 .check((escPressed: number) => {
                     expect(escPressed).toBe(1);
@@ -172,7 +177,7 @@ describe("Groupper - default", () => {
                 })
                 .pressTab()
                 .pressEnter()
-                .activeElement((el) => expect(el?.textContent).toEqual("Foo"))
+                .activeElement((el) => expect(el?.textContent).toEqual("Foo1"))
                 .eval(() => (window as WindowWithEnterFlag).__enterPressed1)
                 .check((escPressed: number) => {
                     expect(escPressed).toBe(0);
@@ -204,7 +209,7 @@ describe("Groupper - default", () => {
                 .pressTab()
                 .pressEnter()
                 .activeElement((el) =>
-                    expect(el?.textContent).toEqual("FooBar")
+                    expect(el?.textContent).toEqual("Foo1Bar1")
                 )
                 .eval(() => (window as WindowWithEnterFlag).__enterPressed2)
                 .check((escPressed: number) => {
@@ -219,20 +224,25 @@ describe("Groupper - limited focus trap", () => {
         await BroTest.bootstrapTabsterPage({ groupper: true });
     });
 
-    const getTestHtml = (tagName: "div" | "li") => {
+    const getTestHtml = (
+        tagName: "div" | "li",
+        isFocusable: boolean,
+        delegated: boolean
+    ) => {
         const rootAttr = getTabsterAttribute({ root: {} });
         const groupperAttr = getTabsterAttribute({
             groupper: {
                 tabbability: Types.GroupperTabbabilities.LimitedTrapFocus,
+                ...(delegated ? { delegated: true } : null),
             },
         });
 
         return (
             <div {...rootAttr}>
-                {groupperItem(tagName, groupperAttr, 1)}
-                {groupperItem(tagName, groupperAttr, 2)}
-                {groupperItem(tagName, groupperAttr, 3)}
-                {groupperItem(tagName, groupperAttr, 4)}
+                {groupperItem(tagName, groupperAttr, isFocusable, 1)}
+                {groupperItem(tagName, groupperAttr, isFocusable, 2)}
+                {groupperItem(tagName, groupperAttr, isFocusable, 3)}
+                {groupperItem(tagName, groupperAttr, isFocusable, 4)}
             </div>
         );
     };
@@ -240,17 +250,17 @@ describe("Groupper - limited focus trap", () => {
     it.each<["div" | "li"]>([["div"], ["li"]])(
         "should focus inside groupper as <%s> with Enter key",
         async (tagName) => {
-            await new BroTest.BroTest(getTestHtml(tagName))
+            await new BroTest.BroTest(getTestHtml(tagName, true, false))
                 .pressTab()
                 .pressEnter()
-                .activeElement((el) => expect(el?.textContent).toBe("Foo"));
+                .activeElement((el) => expect(el?.textContent).toBe("Foo1"));
         }
     );
 
     it.each<["div" | "li"]>([["div"], ["li"]])(
         "should escape focus inside groupper as <%s> with Escape key",
         async (tagName) => {
-            await new BroTest.BroTest(getTestHtml(tagName))
+            await new BroTest.BroTest(getTestHtml(tagName, true, false))
                 .pressTab()
                 .pressEnter()
                 .pressEsc()
@@ -263,13 +273,57 @@ describe("Groupper - limited focus trap", () => {
     it.each<["div" | "li"]>([["div"], ["li"]])(
         "should trap focus within groupper as <%s>",
         async (tagName) => {
-            await new BroTest.BroTest(getTestHtml(tagName))
+            await new BroTest.BroTest(getTestHtml(tagName, true, false))
                 .pressTab()
                 .pressEnter()
                 .pressTab()
-                .activeElement((el) => expect(el?.textContent).toBe("Bar"))
+                .activeElement((el) => expect(el?.textContent).toBe("Bar1"))
                 .pressTab()
-                .activeElement((el) => expect(el?.textContent).toBe("Foo"));
+                .activeElement((el) => expect(el?.textContent).toBe("Foo1"));
+        }
+    );
+
+    it.each<["div" | "li"]>([["div"], ["li"]])(
+        "should trap focus within groupper with not focusable container as <%s>",
+        async (tagName) => {
+            await new BroTest.BroTest(getTestHtml(tagName, false, false))
+                .pressTab()
+                .activeElement((el) => expect(el?.textContent).toBe("Foo1"))
+                .pressTab()
+                .activeElement((el) => expect(el?.textContent).toBe("Bar1"))
+                .pressTab()
+                .activeElement((el) => expect(el?.textContent).toBe("Foo1"))
+                .pressTab()
+                .activeElement((el) => expect(el?.textContent).toBe("Bar1"));
+        }
+    );
+
+    it.each<["div" | "li"]>([["div"], ["li"]])(
+        "should not trap focus within groupper with delegated prop and not focusable container as <%s>",
+        async (tagName) => {
+            await new BroTest.BroTest(getTestHtml(tagName, false, true))
+                .pressTab()
+                .activeElement((el) => expect(el?.textContent).toBe("Foo1"))
+                .pressTab()
+                .activeElement((el) => expect(el?.textContent).toBe("Foo2"))
+                .pressTab()
+                .activeElement((el) => expect(el?.textContent).toBe("Foo3"))
+                .pressTab()
+                .activeElement((el) => expect(el?.textContent).toBe("Foo4"))
+                .pressTab()
+                .activeElement((el) => {
+                    expect(el?.textContent).toBeUndefined();
+                })
+                .pressTab(true)
+                .activeElement((el) => expect(el?.textContent).toBe("Foo4"))
+                .pressTab(true)
+                .activeElement((el) => expect(el?.textContent).toBe("Foo3"))
+                .pressEnter()
+                .activeElement((el) => expect(el?.textContent).toBe("Bar3"))
+                .pressTab()
+                .activeElement((el) => expect(el?.textContent).toBe("Foo3"))
+                .pressTab()
+                .activeElement((el) => expect(el?.textContent).toBe("Bar3"));
         }
     );
 });
