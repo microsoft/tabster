3af75c397c405c876034fa5b3e903992dccc2e8e
Tiger Oakes <contact@tigeroakes.com>
2023-03-30 23:25:28 -0700

    Check if EventTarget is constructable (#261)

--- src/EventTarget.ts
+++ src/EventTarget.ts
@@ -8,8 +8,15 @@ import type { GetWindow } from "./Types";
 export function createEventTarget(getWindow: GetWindow): EventTarget {
     const global = getWindow() as unknown as typeof globalThis;
 
-    if (global.EventTarget) {
-        return new global.EventTarget();
+    try {
+        if (global.EventTarget) {
+            return new global.EventTarget();
+        }
+    } catch (error) {
+        // thrown if EventTarget is not constructable or doesn't exit
+        if (!(error instanceof TypeError)) {
+            throw error;
+        }
     }
 
     return global.document.createElement("div");
