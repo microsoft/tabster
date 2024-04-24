32814f8f703a24e7527c49433d68a83b4cc9d08e
Marat Abdullin <marata@microsoft.com>
2024-04-24 09:59:25 +0200

    feat(Modalizer): Adding semi-internal noDirectAriaHidden flag to tap into the process of setting aria-hidden.

--- src/Types.ts
+++ src/Types.ts
@@ -19,6 +19,15 @@ export const FocusableSelector = [
     "video[controls]",
 ].join(", ");
 
+export interface HTMLElementWithTabsterFlags extends HTMLElement {
+    __tabsterElementFlags?: {
+        noDirectAriaHidden?: boolean; // When Modalizer sets aria-hidden on everything outside of the modal,
+        // do not set aria-hidden directly on this element, go inside and check its children,
+        // and set aria-hidden on the children. This is to be set on a container that hosts
+        // elements which have the active modal dialog as virtual parent.
+    };
+}
+
 export interface TabsterDOMAttribute {
     [TabsterAttributeName]: string | undefined;
 }
