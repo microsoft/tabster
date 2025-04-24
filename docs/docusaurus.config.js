dd15c3cbde1b4e2f2b597673ad749db019d01a1e
Marat Abdullin <marata@microsoft.com>
2025-04-24 09:44:31 +0200

    Bumping deps in docs.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,9 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: "Tabster",
